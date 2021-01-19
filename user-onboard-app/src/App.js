c2e7c2750f621f4aa269d384272dea511b42cdc3
Rex-1031 <Rex@Katherines-iMac.local>
2021-01-18 20:38:10 -0500

    basic form built, some styling added for readability

--- user-onboard-app/src/App.js
+++ user-onboard-app/src/App.js
@@ -1,5 +1,5 @@
-
-import './components/Form'
+import React from 'react'
+import { Form } from './components/Form'
 import './App.css';
 
 function App() {
--- user-onboard-app/src/components/Form.js
+++ user-onboard-app/src/components/Form.js
@@ -2,8 +2,37 @@ import React from 'react'
 
 export function Form() {
     return(
-        <div>
-            
-        </div>
+        <form>
+            <label>Name
+                <input
+                    type='text'
+                    name='name'
+                />
+            </label>
+
+            <label>Email
+                <input 
+                    type='email'
+                    name='email'
+                />
+            </label>
+
+            <label>Password
+                <input 
+                    type='password'
+                    name='password'
+                />
+            </label>
+
+            <label>Terms of Service
+                <input 
+                    type='checkbox'
+                    name='terms'
+                />
+            </label>
+
+            <button>Submit</button>
+
+        </form>
     )
 }
